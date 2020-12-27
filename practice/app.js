9e7a7e57a8a41c6c4d03568b139efa3290557228
krisi01 <chrisisbeast@abv.bg>
2020-12-27 11:42:40 -0800

    init

--- practice/app.js
+++ practice/app.js
@@ -1,38 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.write('<html>')
-    res.write('<head>')
-    res.write('</head>')
-    res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"/><button type=""submit">Send</button></form></body>')
-    res.write('</html>')
-    return res.end();
-  }
-  if (url === '/create-user' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    })
-    req.on('end', () => {
-      const parseBody = Buffer.concat(body).toString().split('=')[1];
-      console.log(parseBody);
-    })
-    res.statusCode = 302;
-    res.setHeader('Location', '/create-user');
-    return res.end();
-  }
-  
-  if (url === '/users') {
-    res.write('<html>')
-    res.write('<head>')
-    res.write('</head>')
-    res.write('<body><ul><li>user1</li><li>user2</li><li>user3</li></ul></body>')
-    res.write('</html>')
-    return res.end();
-  }
-})
+const routes = require('./routes');
 
-server.listen(3001);
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
