e81e1c88ea94ee8dd263b7912f3dd01cb51964d3
krisi01 <chrisisbeast@abv.bg>
2020-12-27 11:38:25 -0800

    practice

--- practice/app.js
+++ practice/app.js
@@ -1,41 +1,38 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Posting</title><head>");
-    res.write("<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>sumbit</button></form></body>");
-    res.write("</html>");
-   return  res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-
-    req.on('data', (info) => {
-      console.log(info);
-      body.push(info);
-    })
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-    })
-
-    
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head>')
+    res.write('</head>')
+    res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"/><button type=""submit">Send</button></form></body>')
+    res.write('</html>')
     return res.end();
   }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title><head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+  if (url === '/create-user' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    })
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString().split('=')[1];
+      console.log(parseBody);
+    })
+    res.statusCode = 302;
+    res.setHeader('Location', '/create-user');
+    return res.end();
+  }
+  
+  if (url === '/users') {
+    res.write('<html>')
+    res.write('<head>')
+    res.write('</head>')
+    res.write('<body><ul><li>user1</li><li>user2</li><li>user3</li></ul></body>')
+    res.write('</html>')
+    return res.end();
+  }
+})
 
 server.listen(3001);
