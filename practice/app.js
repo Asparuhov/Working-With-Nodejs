467fc49084f34448ee9ff2dc6d793c41bb057421
krisi01 <chrisisbeast@abv.bg>
2020-12-29 10:52:20 -0800

    More node/express practicing

--- practice/app.js
+++ practice/app.js
@@ -1,18 +1,21 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
-const path = require('path');
-const adminRouter = require('./Routes/admin');
-const shopRouter = require('./Routes/shop');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin' ,adminRouter)
-
-app.use(shopRouter);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '../', 'views', 'error.html'))
-})
-
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3001);
