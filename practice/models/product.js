394ecdb28bcf1baf2665e3db5649c19d3200441c
krisi01 <chrisisbeast@abv.bg>
2021-01-01 07:01:49 -0800

    practicing node

--- practice/models/product.js
+++ practice/models/product.js
@@ -1,32 +1,39 @@
-const {static} = require("express");
+const fs = require('fs');
 const path = require('path');
-const fs = require("fs");
+
 const p = path.join(
-    path.dirname(require.main.filename),
-    "data",
-    "products.json"
-  );
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, data) => {
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    cb(JSON.parse(data));
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
+
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
   }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
