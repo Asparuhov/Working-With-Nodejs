eeabafdd351c2267afef421b224a1639b29eb37c
krisi01 <chrisisbeast@abv.bg>
2021-01-01 07:40:06 -0800

    working with dynamic routes

--- practice/controllers/shop.js
+++ practice/controllers/shop.js
@@ -10,11 +10,16 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getProductId = (req, res, next) => {
+exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  res.redirect('/');
-  console.log(prodId);
-}
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
