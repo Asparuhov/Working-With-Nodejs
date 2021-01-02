9f1d03aa8a609f90abe8eded34443129b66f09a6
krisi01 <chrisisbeast@abv.bg>
2021-01-02 15:33:40 -0800

    Using relations in SQL

--- practice/app.js
+++ practice/app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
-
+const Product = require("./models/product");
+const User = require("./models/user");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,14 +18,36 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
   .sync()
   .then((res) => {
-    app.listen(3000);
+    return User.findByPk(1)
+      .then((user) => {
+        if (!user) {
+          User.create({ name: "Chris", email: "test@test.com" });
+        }
+        return user;
+      })
+      .then((user) => {
+        console.log(user);
+        app.listen(3000);
+      });
   })
   .catch((err) => console.log(err));
